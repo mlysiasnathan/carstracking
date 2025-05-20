2237f527d53d82a3bdc1ef64f9453a68ff2f4152
mlysiasnathan/NatCoding <mlysiasnathan@gmail.com>
2025-05-20 12:49:08 +0200

    refactor: update package name to com.natcoding.carstracking

--- android/app/src/main/kotlin/com/example/nrep/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.natcoding.nrep
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/natcoding/carstracking/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.natcoding.carstracking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() 
